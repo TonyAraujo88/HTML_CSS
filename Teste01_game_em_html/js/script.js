1724658c015a64bf21923718e8b0bb8d49f73602
TonyAraujo88 <tonybugattieb@gmail.com>
2024-07-07 00:53:55 -0300

    Game mudado para dino

--- Teste01_game_em_html/js/script.js
+++ Teste01_game_em_html/js/script.js
@@ -1,32 +1,32 @@
-const mario = document.querySelector('.mario');
+const dino = document.querySelector('.dino');
 const pipe = document.querySelector('.pipe');
 
 const jump = () => {
-    mario.classList.add('jump');
+    dino.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump');
+       dino.classList.remove('jump');
     }, 500);
 }
 
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const dinoPosition = +window.getComputedStyle(dino).bottom.replace('px','');
 
-    console.log(marioPosition);
+    console.log(dinoPosition);
     
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 65) {
+    if (pipePosition <= 120 && pipePosition > 0 && dinoPosition < 65) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        dino.style.animation = 'none';
+        dino.style.bottom = `${dinoPosition}px`;
 
-        mario.src = './imagens/game-over.gif';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+        dino.src = './imagens/game-over.gif';
+        dino.style.width = '75px'
+        dino.style.marginLeft = '50px'
         clearInterval(loop);
     }
 
